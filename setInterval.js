c59c3fba1155bc2c5c1fd0efc1cebceaaac9bb8e
abhishekbindraDev <46415973+abhishekbindraDev@users.noreply.github.com>
2019-01-06 20:11:43 +0530

    Add files via upload

---
+++ setInterval.js
@@ -0,0 +1,22 @@
+
+    setInterval(function(){
+      if(document.querySelectorAll(".navbar_fixed").length>0){
+        console.log("Cool");
+        document.querySelector(".navbar").style.background="white";
+        var x1=document.querySelectorAll(".nav-item .nav-link");
+        for(var i=0;i<x1.length;i++){
+          x1[i].style.color="#4F2B4F";
+        }
+        document.querySelector(".logo_h").style.color="#4F2B4F";
+        document.querySelector(".logo_h img").src="image/logo_.png";
+
+      }
+      else{
+        console.log("Hello");document.querySelector(".navbar").style.background="transparent";var x=document.querySelectorAll(".nav-item .nav-link");
+        for(var i=0;i<x.length;i++){
+          x[i].style.color="white";
+        }
+        document.querySelector(".logo_h").style.color="white";
+document.querySelector(".logo_h img").src="image/logo_1w.png";
+    }
+  },500);
